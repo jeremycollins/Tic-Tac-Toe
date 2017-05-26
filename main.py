44bbd1d03b7d7e1f28a900d91d45bac990b9d389
Jeremy Collins <jeremy@jeremycollins.me>
2017-05-26 18:51:58 -0500

    Created tic-tac-toe board template

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+def display_board(board):
+    os.system('clear')
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+
+board = [0, 'X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X']
+display_board(board)
